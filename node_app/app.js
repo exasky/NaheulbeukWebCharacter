f2b813e2fb3e2a5d32614f937c3e2a47ecc39596
Simar Jérémy <exasky@live.fr>
2017-07-19 16:38:08 +0000

    Finalize back end

--- node_app/app.js
+++ node_app/app.js
@@ -12,6 +12,7 @@ var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- node_app/routes/characters.js
+++ node_app/routes/characters.js
@@ -1,59 +1,90 @@
-var express = require('express');
-var router = express.Router();
-var MongoClient = require("mongodb").MongoClient
+var router = require('express').Router();
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
 
-var dbName = "mongodb://localhost/testCharacter";
+var dbUrl = "mongodb://localhost/testCharacter";
 var dbCollection = "test";
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  console.log(req.query);
-  console.log(dbName);
-  console.log(dbCollection);
-  if (req.query && req.query.name) {
-    console.log(req.query.name);
-    MongoClient.connect(dbName, function (error, db) {
-      if (error) return funcCallback(error);
-      db.collection(dbCollection).findOne({ name: req.query.name }, function (error, result) {
-        if (error) throw error;
-        console.log("Trying to find character");
-        console.log(result);
-        if (result) {
-          console.log(
-            "ID : " + result._id.toString() + "\n" +
-            "Nom : " + result.name + "\n" +
-            "Jeu : " + result.game
-          );
-          res.write(JSON.stringify(result));
-        }
-        res.end();
-      });
-    });
-  }
-});
+var database;
 
-router.get('/all', function (req, res, next) {
-  MongoClient.connect("mongodb://localhost/testCharacter", function (error, db) {
-    if (error) return funcCallback(error);
-    db.collection("test").find({}, {"name" : 1, "_id" : 0}).toArray(function(error, result) {
-      res.write(JSON.stringify(result));
-      res.end();
-    });
-  });
-});
-
-router.post('/', function (req, res) {
-  console.log(req.body);
-  if (req.body) {
-    MongoClient.connect(dbName, function (error, db) {
-      if (error) return funcCallback(error);
-      db.collection(dbCollection).save(req.body, null, function (error, results) {
-        if (error) throw error;
-        console.log("Le document a bien été inséré");
-        res.end();
-      });
-    });
-  }
+MongoClient.connect(dbUrl, (err, db) => {
+  if (err) return console.log(err)
+  database = db;
+  console.log("[MONGO] Connected to db: " + dbUrl);
 })
+
+// GET : get one
+router.get('/:characterId', function (req,res){
+    console.log("character details : " + req.params.characterId);
+    if (req.params.characterId) {
+        database.collection(dbCollection).findOne({_id : new ObjectId(req.params.characterId)}, (err, result) => {
+            if (err) processError(err, res);
+            res.write(JSON.stringify(result));
+            res.end();
+        });
+    } else {
+        processError("No id defined in get request", res);
+    }
+});
+
+// GET : get all
+router.get('/', function (req,res){
+    database.collection(dbCollection).find().toArray(function(err, result) {
+        if (err) processError(err, res);
+        res.write(JSON.stringify(result));
+        res.end();
+    });
+});
+
+// POST : create
+router.post('/', function(req, res){
+    if (req.body) {
+        console.log('character created');
+        database.collection(dbCollection).insert(req.body, (err, result) => {
+            if (err) processError(err, res);
+            console.log('saved to database: ' + JSON.stringify(result.insertedIds));
+            res.write(JSON.stringify(result.insertedIds));
+            res.end();
+        });
+    } else {
+        processError("No input defined in create request", res);
+    }
+});
+
+// PUT : update one
+router.put('/:characterId', function(req, res){
+    console.log('character updated : ' + req.params.characterId);
+    if (req.params.characterId) {
+        var character = req.body;
+        character._id = new ObjectId(req.params.characterId);
+        database.collection(dbCollection).save(character, (err, result) => {
+            if (err) processError(err, res);
+            console.log('saved to database');
+            res.end();
+        });
+    } else {
+        processError("No id defined in update request", res);
+    }
+})
+
+// DELETE : delete
+router.delete('/:characterId', function(req, res){
+    if (req.params.characterId) {
+        console.log('character deleted : ' + req.params.characterId)
+        database.collection(dbCollection).remove({_id : new ObjectId(req.params.characterId)}, (err, result) => {
+            if (err) processError(err, res);
+            res.send('character deleted');
+            res.end();
+        });
+    } else {
+        processError("No id defined in delete request", res);
+    }
+});
+
+function processError(error, response) {
+    console.log(error);
+    response.send(500, error);
+    response.end();
+}
 
 module.exports = router;
