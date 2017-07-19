a58931a8a73a63e8dd0c9b886a20b3a8616dc9f0
Jérémy Simar <simar.jeremy@live.fr>
2017-07-19 20:45:18 +0200

    front complete

--- aurelia_app/src/character/character.js
+++ aurelia_app/src/character/character.js
@@ -23,11 +23,47 @@ export class Character {
         } else {
           this.character = character;
           this.heading = "Fiche personnage";
+          this.initCharacter();
         }
       });
     } else {
       this.heading = "Création de personnage";
+      this.initCharacter();
     }
+  }
+
+  initCharacter() {
+    // if (!this.character.characteristics) {
+    //   this.character.characteristics = [];
+    // }
+    // for (var i = this.character.characteristics.length; i < 5; i++) {
+    //   this.character.characteristics[i] = {};
+    // }
+    this.character.characteristics = this.initArray(this.character.characteristics);
+
+    // if (!this.character.fightCharacteristics) {
+    //   this.character.fightCharacteristics = [];
+    // }
+    // for (var i = this.character.fightCharacteristics.length; i < 5; i++) {
+    //   this.character.fightCharacteristics[i] = {};
+    // }
+    this.character.fightCharacteristics = this.initArray(this.character.fightCharacteristics, 5);
+
+    this.character.defItems = this.initArray(this.character.defItems, 6);
+
+    this.character.attItems = this.initArray(this.character.attItems, 4);
+    this.character.preciousItems = this.initArray(this.character.preciousItems, 10);
+    this.character.items = this.initArray(this.character.items, 15);
+  }
+
+  initArray(element, size) {
+    if (!element) {
+      element = [];
+    }
+    for (var i = element.length; i < size; i++) {
+      element[i] = {};
+    }
+    return element;
   }
 
   save() {
